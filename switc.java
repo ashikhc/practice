a546258c04e90a5427e8f84a16cd3ef6dde43bcf
ashikhc <ashik357@gmail.com>
2016-12-27 19:47:57 +0530

    switch

---
+++ switc.java
@@ -0,0 +1,30 @@
+class switc
+{
+	public static void main(String args[])
+	{
+		
+     char direction  = 'S';
+
+switch (direction)
+{
+case 'E': System.out.println(" east");
+            break;
+			
+case 'W': System.out.println("west");
+           break;
+
+case 'S' : System.out.println("south");
+           break;
+		   
+		   case 's' : System.out.println("southieeee");
+           break;
+
+case 'N' : System.out.println("North");
+            break;
+
+default : System.out.println("waste");
+            break;			
+	 }
+		
+	}
+}
